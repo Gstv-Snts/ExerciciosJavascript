e61b33fce086ddf401a14306be383c77bad74afb
Gustavo Santos <gustavosantossilveira12360@gmail.com>
2022-07-05 21:28:59 -0300

    Exercicio 1

---
+++ Exercicio1/index.js
@@ -0,0 +1,11 @@
+const numero = document.querySelector("input");
+const button = document.querySelector("button");
+const h1 = document.querySelector("h1");
+let vezesClicadas = 0;
+h1.innerText = 0;
+let sum = 0;
+button.addEventListener("click", () => {
+  vezesClicadas++;
+  sum += parseInt(numero.value);
+  h1.innerText = (sum / vezesClicadas).toFixed(1);
+});
