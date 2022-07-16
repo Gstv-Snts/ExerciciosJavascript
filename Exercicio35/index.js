adad3a412fbc1917c222d47ba16dcf63f29c9f4c
Gustavo Santos <gustavosantossilveira12360@gmail.com>
2022-07-15 21:40:25 -0300

    Exercicio 35

---
+++ Exercicio35/index.js
@@ -0,0 +1,6 @@
+const deuses = ["Aegir", "Aud", "Balder"];
+
+console.log(`O array deuses tem ${deuses.length} deuses!`);
+for (let i = 0; i < deuses.length; i++) {
+  console.log(`O deus numero ${i + 1} tem ${deuses[i].length} characteres!`);
+}
